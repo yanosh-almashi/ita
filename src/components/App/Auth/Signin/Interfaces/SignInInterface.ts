97b7aecd8cea270379e3f3dd3202b679b04d03e6
alina_laktionova <alinalak18for@gmail.com>
2020-01-13 13:51:26 +0200

    edit interfaces

--- src/components/App/Auth/Signin/Interfaces/SignInInterface.ts
+++ src/components/App/Auth/Signin/Interfaces/SignInInterface.ts
@@ -1,6 +1,4 @@
 export interface SigninInterface {
-  uid: string | null;
-  token: string | null;
   error: string | null;
   signInUser: (email: string, password: string) => void;
   signOutUser: () => void;
