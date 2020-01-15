e877916331373d53ff43ffa2e3f92b015303bab1
Andrey Tkachenko <andreytk9696@gmail.com>
2020-01-15 21:31:25 +0200

    fixed some bugs with open/close state of menu

--- src/components/App/MenuContainer/NavigationMenuList/NavigationMenuList.tsx
+++ src/components/App/MenuContainer/NavigationMenuList/NavigationMenuList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import MenuItem from "./NavigationMenuItem/NavigationMenuItem";
 import styled from "styled-components";
 import { ItemsInterface, NestedMenuType } from "../types/types";
@@ -10,24 +10,32 @@ interface Props {
 }
 
 const NavigationMenu: React.FC<Props> = (props: Props) => {
+  const [active, setActive] = useState<boolean>(false);
+
   const StyledNavMenu = styled.ul`
     margin-top: ${props.addNestedMenu ? "40px" : "93px"};
   `;
 
+  const activeState = (elem: boolean) => {
+    setActive(elem);
+  };
+
   return (
-    <StyledNavMenu>
-      {props.menuItems.map((item: ItemsInterface, i: number) => (
-        <MenuItem
-          key={i + item.name}
-          isNestedItem={props.nestedRoute!}
-          name={item.name}
-          path={props.nestedRoute ? props.nestedRoute + item.path : item.path}
-          icon={item.icon}
-          nextMenu={item.nextMenu!}
-          addNestedMenu={props.addNestedMenu}
-        />
-      ))}
-    </StyledNavMenu>
+      <StyledNavMenu>
+        {props.menuItems.map((item: ItemsInterface, i: number) => (
+            <MenuItem
+                key={i + item.name}
+                isNestedItem={props.nestedRoute!}
+                name={item.name}
+                path={props.nestedRoute ? props.nestedRoute + item.path : item.path}
+                icon={item.icon}
+                nextMenu={item.nextMenu!}
+                addNestedMenu={props.addNestedMenu}
+                active={active}
+                setActive={activeState}
+            />
+        ))}
+      </StyledNavMenu>
   );
 };
 
