04a1915372854ac40f76b05bb4eec74431ee0610
Kostiantyn Bondarenko <constantinebond.new@gmail.com>
2020-01-21 12:00:43 +0200

    added many-to-many logic

--- src/components/App/Randomizer/Randomizer.tsx
+++ src/components/App/Randomizer/Randomizer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 const StyledRandom = styled.div`
   width: 100%;
@@ -36,40 +36,68 @@ const RandomResultsContainer = styled.div`
  margin: 0 auto 30px auto;
  border-bottom: 1px solid #dedde3;
  color: #20233f;
- font-size: 25px;`;
+ font-size: 25px;
+ `;
 
+ const ResultUnmarkedList = styled.ul`
+ list-style: none;
+ `;
+
+
+
+ interface SomeInterface{
+   id: number;
+   value: string;
+ }
 export default function MultilineTextFields() {
 
   const [value, setValue] = React.useState('Your result will be here');
-  const [result, setResult] = React.useState();
-
+  const [result, setResult] = React.useState<SomeInterface[]>([]);
+  const [inputValue, setInputValue]= React.useState(1);;
+  
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  let resultArray = [];
+  //TYPE IS ANY DUE TO MATERIAL UI DOCUMENTATION ISSUES
+  const handleInputChange = (event: any) => {
+    setInputValue(Number(event.target.value));
+    console.log(inputValue);
+    console.log(typeof(inputValue));
+  };
+ 
+  let resultArray: string[] = [];
+  let numberOfItems: number = 2;
+
   const randomize = () =>
   {
-    resultArray = value.split('\n');
-  
-      let currentIndex = resultArray.length, temporaryValue, randomIndex, resultIndex;
-    
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-    
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-    
-        // And swap it with the current element.
-        temporaryValue = resultArray[currentIndex];
-        resultArray[currentIndex] = resultArray[randomIndex];
-        resultArray[randomIndex] = temporaryValue;
-      }
-      resultIndex = Math.floor(Math.random() * resultArray.length);
-      setResult(resultArray[resultIndex]);
-  }
+    let currentIndex = resultArray.length, 
+    temporaryValue, randomIndex, resultIndex, arr=[];
 
+
+    setResult([]);
+    for(let i = 0; i<inputValue; i++){
+    resultArray = value.split('\n');
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          // And swap it with the current element.
+          temporaryValue = resultArray[currentIndex];
+          resultArray[currentIndex] = resultArray[randomIndex];
+          resultArray[randomIndex] = temporaryValue;
+        }
+      resultIndex = Math.floor(Math.random() * resultArray.length);
+      let ItemOfResult = {id: 0, value: ""};
+      ItemOfResult.value = resultArray[resultIndex];
+      ItemOfResult.id = Math.random()*1e8;
+      
+
+      arr.push(ItemOfResult); 
+    } 
+    setResult( [...arr]);
+  }
   return (
   <StyledRandom>
         <RandomControlContainer>
@@ -82,13 +110,21 @@ export default function MultilineTextFields() {
             variant="outlined"
             onChange={handleChange}
             />
+            <Input type="number" color="primary" onChange={handleInputChange}></Input>
             <Button variant="contained" color="primary" onClick={randomize}>
             Randomize
             </Button>
         </RandomControlContainer>
         <div className="result_container">
         <RandomTitleContainer>Result area</RandomTitleContainer>
-        <RandomResultsContainer>{result}</RandomResultsContainer>
+        <RandomResultsContainer>
+          <ResultUnmarkedList>
+            {result.map((item) => {
+              return <li key={item.id}>{item.value}</li>     
+              })
+            }
+          </ResultUnmarkedList>
+        </RandomResultsContainer>
         </div>
     </StyledRandom>
   );
