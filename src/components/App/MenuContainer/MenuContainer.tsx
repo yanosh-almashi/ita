7b0e579bdc2284390c8aa65692d0825cf8a5f320
letrylesha <lunarboxnote@gmail.com>
2020-01-22 23:27:56 +0300

    bugfix

--- src/components/App/MenuContainer/MenuContainer.tsx
+++ src/components/App/MenuContainer/MenuContainer.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './MenuContainer.css';
 import NavigationMenu from './NavigationMenu/NavigationMenu';
 
-const MenuContainer = () => {
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { signOutUser } from "../../../store/auth/actionCreators";
+
+const MenuContainer = ({ signOutUser }: any) => {
   return (
     <div className="menu-container">
       <div className="menu" >
+        <button onClick={signOutUser}>SIGNOUT</button>
         <NavigationMenu />
       </div>
     </div>
   )
 } 
 
-export default MenuContainer;
+const mapDispatchToProps = (dispatch: any) => {
+  return bindActionCreators({ signOutUser }, dispatch);
+};
+
+export default connect(null, mapDispatchToProps)(MenuContainer);
