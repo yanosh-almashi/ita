e87524d42e3cf8a2a0050fe36e8cfa47f6459d09
letrylesha <lunarboxnote@gmail.com>
2020-01-24 15:23:42 +0300

    bug fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import MenuContainer from './MenuContainer/MenuContainer';
 import ContentWrapper from './ContentWrapper/ContentWrapper';
-import AuthPopUp from './Auth/Auth';
 
 const App = () => {
   return (
     <div className="App">
-      <AuthPopUp />
       <MenuContainer />
       <ContentWrapper />
     </div>
--- src/components/App/Auth/firebase.config.ts
+++ src/components/App/Auth/firebase.config.ts
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  authDomain: 'itatools35.firebaseapp.com',
+  databaseURL: 'https://itatools35.firebaseio.com',
+  projectId: 'itatools35',
+  storageBucket: 'itatools35.appspot.com',
+  messagingSenderId: '370129293158',
+  appId: '1:370129293158:web:6aadf1e773140d4b10c2a7'
 };
 
 export const Firebase = firebase.initializeApp(firebaseConfig);
