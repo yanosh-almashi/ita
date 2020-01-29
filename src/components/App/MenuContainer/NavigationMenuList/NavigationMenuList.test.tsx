f43350eb56e5b69e5aea79391dbfd477ecc5c85d
Andrey Tkachenko <andreytk9696@gmail.com>
2020-01-29 19:11:05 +0200

    Fixed tests

--- src/components/App/MenuContainer/NavigationMenuList/NavigationMenuList.test.tsx
+++ src/components/App/MenuContainer/NavigationMenuList/NavigationMenuList.test.tsx
@@ -3,6 +3,11 @@ import { render } from '@testing-library/react';
 import NavigationMenu from './NavigationMenuList';
 import { ItemsInterface } from '@components/App/MenuContainer/types/types';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from "react-redux";
+import configureStore, {MockStore} from "redux-mock-store";
+
+
+const mockStore = configureStore([]);
 
 const fakeItems: ItemsInterface[] = [
   {
@@ -18,10 +23,23 @@ const fakeItems: ItemsInterface[] = [
 ];
 
 describe('NavMenuList', () => {
+  let store: MockStore;
+
+  beforeEach(() => {
+    store = mockStore({
+      authReducer: {
+        uid: 'token'
+      }
+    });
+    store.dispatch = jest.fn();
+  });
+
   it('test', () => {
     const { container } = render(
         <BrowserRouter>
-          <NavigationMenu menuItems={fakeItems} />
+          <Provider store={store}>
+            <NavigationMenu menuItems={fakeItems} />
+          </Provider>
         </BrowserRouter>
         );
     const navMenuItems = container.querySelector('ul');
