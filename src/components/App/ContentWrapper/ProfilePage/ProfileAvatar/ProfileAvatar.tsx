b1ed343430ae706cb733964dd8d8c569a0216df2
letrylesha <lunarboxnote@gmail.com>
2020-01-29 18:13:23 +0300

    added log out

--- src/components/App/ContentWrapper/ProfilePage/ProfileAvatar/ProfileAvatar.tsx
+++ src/components/App/ContentWrapper/ProfilePage/ProfileAvatar/ProfileAvatar.tsx
@@ -9,6 +9,7 @@ import {
 } from '../ProfilePageStyles';
 import { connect } from 'react-redux';
 import { changeProfileSummaryWindow } from '../../../../../store/profile/ProfileActions';
+import { signOutUser } from '../../../../../store/auth/actionCreators';
 import Img from '../../../../../assets/images/ava.jpg';
 
 interface Props {
@@ -17,10 +18,11 @@ interface Props {
   windowStatus: boolean;
   url: string;
   changeProfileSummaryWindow: () => void;
+  signout: () => void;
 }
 
 const ProfileAvatar: React.FC<Props> = props => {
-  const { url, name, group, changeProfileSummaryWindow, windowStatus } = props;
+  const { url, name, group, changeProfileSummaryWindow, windowStatus, signout } = props;
 
   return (
     <ProfileAvatarContainer>
@@ -32,12 +34,16 @@ const ProfileAvatar: React.FC<Props> = props => {
       <ProfileSwipeButton onClick={() => changeProfileSummaryWindow()}>
         {windowStatus ? 'Edit profile' : 'Profile summary'}
       </ProfileSwipeButton>
+      <ProfileSwipeButton onClick={() => signout()}>
+        Log out
+      </ProfileSwipeButton>
     </ProfileAvatarContainer>
   );
 };
 
 const mapDispatchToProps = (dispatch: any) => ({
-  changeProfileSummaryWindow: () => dispatch(changeProfileSummaryWindow())
+  changeProfileSummaryWindow: () => dispatch(changeProfileSummaryWindow()),
+  signout: () => dispatch(signOutUser())
 });
 
 const mapStateToProps = (state: any) => ({
